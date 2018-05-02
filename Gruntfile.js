7b99051f315941d1270529d3dd9b88d0fda04dc5
Bodhi Hu <huhuaishun@icloud.com>
2018-05-02 17:33:15 +0800

    update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,29 @@
 
 const sh = require('shelljs');
 const _ = require('underscore');
+const debug = require('debug')('Deva');
+const colors = require('colors');
 const CONFIG = require('./Project');
 
+sh.config.verbose = true;
+
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
   });
+
   grunt.registerTask("pullRebaseMaster", function() {
-    sh.echo(`TODO: pullRebaseMaster`);
     const moduleKeys = _.keys(CONFIG.modules);
     moduleKeys && moduleKeys.forEach((key) => {
-      const module = CONFIG.modules[key];
+
+      const { path, remote } = CONFIG.modules[key];
+      console.log(`\n[Deva] Pull & Rebase ${key}\n`.underline.green);
+      if (sh.cd(`${path}`).code === 0) {
+        sh.exec(`git checkout master`);
+        sh.exec(`git pull ${remote} master --rebase`);
+      }
+
     });
   });
 };
